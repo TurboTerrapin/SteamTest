db14aacdb6f0879bafebac09902a3777a1d5af00
Henryk M. <musial@uw.edu>
2025-09-03 00:12:58 -0700

    Coalesced CollisionDetection scripts into single CollisionSystem

--- Assets/Scripts/ShipScripts/ShipController.cs
+++ Assets/Scripts/ShipScripts/ShipController.cs
@@ -1,23 +1,26 @@
-using Unity.Netcode;
+using Unity.Netcode; 
 using UnityEngine;
 
 [RequireComponent(typeof(PilotingSystem))]
 [RequireComponent(typeof(WeaponsSystem))]
+[RequireComponent(typeof(CollisionSystem))]
+
 public class ShipController : NetworkBehaviour
 {
     private PilotingSystem pilotingSystem;
     private WeaponsSystem weaponsSystem;
+    private CollisionSystem collisionSystem;
 
     private GameObject controlHandler;
     private bool shipReady = false;
 
-    public GameObject worldRoot; 
+    public GameObject worldRoot;
 
     private void Awake()
     {
         pilotingSystem = GetComponent<PilotingSystem>();
         weaponsSystem = GetComponent<WeaponsSystem>();
-
+        //collisionSystem = GetComponent<CollisionSystem>();
     }
 
     void Start()
@@ -25,8 +28,10 @@ public class ShipController : NetworkBehaviour
         controlHandler = GameObject.FindGameObjectWithTag("ControlHandler");
         worldRoot = GameObject.FindGameObjectWithTag("WorldRoot");
 
-        if (controlHandler != null && worldRoot != null && pilotingSystem.AssignControlReferences(controlHandler)
-            && weaponsSystem.AssignControlReferences(controlHandler))
+        if (controlHandler != null &&
+                worldRoot != null &&
+                    pilotingSystem.AssignControlReferences(controlHandler) &&
+                        weaponsSystem.AssignControlReferences(controlHandler)) 
         {
             shipReady = true;
         }
