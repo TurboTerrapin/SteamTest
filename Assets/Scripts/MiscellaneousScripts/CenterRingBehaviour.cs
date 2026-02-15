f2b28a5eb32a685c31743fd8150fdf40670764e2
John Aylward <turboterrapin@gmail.com>
2026-02-15 00:59:40 -0800

    Changed the way that the red light green light rings work

--- Assets/Scripts/MiscellaneousScripts/CenterRingBehaviour.cs
+++ Assets/Scripts/MiscellaneousScripts/CenterRingBehaviour.cs
@@ -1,32 +1,99 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class CenterRingBehaviour : MonoBehaviour
 {
+
+    [SerializeField]
+    private bool doesPulse = false;
+    [SerializeField]
     float Speed = 15f;
+    [SerializeField]
     float MaxScale = 550f;
+    [SerializeField]
     float MinScale = 500f;
+    [SerializeField]
+    float ScaleStep = 0f;
+    [SerializeField]
+    float time = 0f;
+    [SerializeField]
     bool isScaling = true;
+    [SerializeField]
+    private AnimationCurve curve;
+
+    [SerializeField]
+    private bool doesRotate = false;
+    [SerializeField]
+    private float rotationSpeed = 18f;
+    [SerializeField]
+    private bool rotatesUsingGlobalAxis = true;
+    [SerializeField]
+    private Vector3 axis = new Vector3(0, 1, 0);
 
     void Update()
     {
-        if (isScaling)
+        if (doesPulse)
         {
-            transform.localScale += Vector3.one * Speed * Time.deltaTime;
-
-            if (transform.localScale.x >= MaxScale)
+            if (isScaling)
             {
-                isScaling = false;
+                time += Time.deltaTime * Speed / 60;
+
+
+                if (time >= 1)
+                {
+                    isScaling = false;
+                }
+
+                //transform.localScale += Vector3.one * Speed * Time.deltaTime;
+
+                //if (transform.localScale.x >= MaxScale)
+                //{
+                //    isScaling = false;
+                //}
             }
+            else
+            {
+                time -= Time.deltaTime * Speed / 60;
+
+                if (time <= 0)
+                {
+                    isScaling = true;
+                }
+
+                //transform.localScale -= Vector3.one * Speed * Time.deltaTime;
+
+                //if (transform.localScale.x <= MinScale)
+                //{
+                //    isScaling = true;
+                //}
+            }
+
+            ScaleStep = MaxScale - MinScale;
+
+
+            transform.localScale = Vector3.one * (MinScale + (curve.Evaluate(time) * ScaleStep));
         }
-        else
+
+        if (doesRotate)
         {
-            transform.localScale -= Vector3.one * Speed * Time.deltaTime;
-
-            if (transform.localScale.x <= MinScale)
+            axis.Normalize();
+            if (rotatesUsingGlobalAxis)
             {
-                isScaling = true;
+                transform.localRotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, transform.InverseTransformDirection(axis));
             }
+            else
+            {
+                transform.localRotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, axis);
+            }
+            //transform.InverseTransformDirection(Vector3.up);
+
         }
+
+
+
+
+
 
     }
 }
+
